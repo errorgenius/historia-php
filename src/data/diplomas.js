a36e7b35604ff37e0e080d5a3d53bc2151c6c564
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2024-05-08 22:04:24 -0400

    v1

---
+++ src/data/diplomas.js
@@ -0,0 +1,142 @@
+const TAGS = {
+    ANALISTA: {
+      name: "Analista",
+    },
+    PROGRAMADOR: {
+      name: "Programador",
+    },
+    DATOS: {
+      name: "Base de Datos",
+    },
+    TEST: {
+      name: "Probador",
+    },
+  };
+  
+  export const DIPLOMAS = [
+    {
+      title: "Técnico Medio en Informaticas",
+      image: "/diplomas/Tecnico-medio.jpeg",
+      tags: [TAGS.ANALISTA, TAGS.PROGRAMADOR, TAGS.DATOS, TAGS.TEST],
+    }, 
+    {
+      title: "PILA DESARROLLO DE APLICACIONES WEB",
+      link: "/diplomas/piladaw.png",
+      image: "/diplomas/piladaw.png",
+      tags: [TAGS.ANALISTA, TAGS.PROGRAMADOR],
+    },
+    {
+      title: "Admisión a Desarrollo Web en Henry",
+      link: "/diplomas/henry.png",
+      image: "/diplomas/henry.png",
+      tags: [TAGS.PROGRAMADOR],
+    },
+    //analista
+    {
+      title: "Participante Copa de ingeniería de Software",
+      link: "/diplomas/analista/copa-isw-p.jpg",
+      image: "/diplomas/analista/copa-isw-p.jpg",
+      tags: [TAGS.ANALISTA],
+    },
+    {
+      title: "Primer Lugar Copa de ingeniería de Software",
+      link: "/diplomas/analista/copa-isw1.jpg",
+      image: "/diplomas/analista/copa-isw1.jpg",
+      tags: [TAGS.ANALISTA],
+    },
+    {
+      title: "Tercer Lugar Copa de ingeniería de Software",
+      link: "/diplomas/analista/copa-isw3.jpg",
+      image: "/diplomas/analista/copa-isw3.jpg",
+      tags: [TAGS.ANALISTA],
+    },
+    {
+      title: "Participante Copa de ingeniería de Software",
+      link: "/diplomas/analista/copa-jc-isw-p.jpg",
+      image: "/diplomas/analista/copa-jc-isw-p.jpg",
+      tags: [TAGS.ANALISTA],
+    },
+    {
+      title: "Tercer Lugar Copa de ingeniería de Software",
+      link: "/diplomas/analista/copa-jc-isw3.jpg",
+      image: "/diplomas/analista/copa-jc-isw3.jpg",
+      tags: [TAGS.ANALISTA],
+    },
+    {
+      title: "Fundamentos en Gestión de Proyectos",
+      link: "/diplomas/analista/gpi.png",
+      image: "/diplomas/analista/gpi.png",
+      tags: [TAGS.ANALISTA],
+    },
+    {
+      title: "Scrum Foundation Professional Certification",
+      link: "/diplomas/analista/scrum.jpg",
+      image: "/diplomas/analista/scrum.jpg",
+      tags: [TAGS.ANALISTA],
+    },
+    {
+      title: "Scrum Fundamentals Certified",
+      link: "/diplomas/analista/scrum4.png",
+      image: "/diplomas/analista/scrum4.png",
+      tags: [TAGS.ANALISTA],
+    },
+    //Base de datos
+    {
+      title: "Participante Copa de Base de Datos",
+      link: "/diplomas/base_de_datos/copa-bd-p.jpg",
+      image: "/diplomas/base_de_datos/copa-bd-p.jpg",
+      tags: [TAGS.ANALISTA, TAGS.DATOS],
+    },
+    {
+      title: "Tercer Lugar Copa de Base de Datos",
+      link: "/diplomas/base_de_datos/copa-bd3.jpg",
+      image: "/diplomas/base_de_datos/copa-bd3.jpg",
+      tags: [TAGS.ANALISTA, TAGS.DATOS],
+    },
+    //Programador
+    {
+      title: "Alumno Ayudante de IP1",
+      link: "/diplomas/Programador/aa-ip1.jpg",
+      image: "/diplomas/Programador/aa-ip1.jpg",
+      tags: [TAGS.ANALISTA, TAGS.PROGRAMADOR],
+    },
+    {
+      title: "Alumno Ayudante de IP2",
+      link: "/diplomas/Programador/aa-ip2.jpg",
+      image: "/diplomas/Programador/aa-ip2.jpg",
+      tags: [TAGS.ANALISTA, TAGS.PROGRAMADOR],
+    },
+    {
+      title: "Participante Copa Pascal de Programación",
+      link: "/diplomas/Programador/copa-pascal-p.jpg",
+      image: "/diplomas/Programador/copa-pascal-p.jpg",
+      tags: [TAGS.ANALISTA, TAGS.PROGRAMADOR],
+    },
+    {
+      title: "Tercer Lugar Copa Pascal de Programación",
+      link: "/diplomas/Programador/copa-pascal3.jpg",
+      image: "/diplomas/Programador/copa-pascal3.jpg",
+      tags: [TAGS.ANALISTA, TAGS.PROGRAMADOR],
+    },
+    //EDTeam
+    {
+      title: "CertificateS EDTeam",
+      link: "https://drive.google.com/drive/folders/1qe4X6c1VwxzZ0mT_UWSpaDclaI68MEZ0?usp=drive_link",
+      image: "/diplomas/EDTeam/desarrollo-personal.png",
+      tags: [TAGS.ANALISTA, TAGS.PROGRAMADOR, TAGS.DATOS],
+    },
+    //Platzi
+    {
+      title: "CertificateS Platzi",
+      link: "https://drive.google.com/drive/folders/1LQ4ANNlEG7KbMkGJLLlpSB_6X2TWR-UY?usp=drive_link",
+      image: "/diplomas/Platzi/Diplomas_del_Curso_de_Fundamentos_de_Python.png",
+      tags: [TAGS.PROGRAMADOR],
+    },
+    //SoloLearn
+    {
+      title: "CertificateS SoloLearn",
+      link: "https://drive.google.com/drive/folders/1EqG4MEDa1nJ-czQytEAqN5DPwpKeGSNt?usp=drive_link",
+      image: "/diplomas/SoloLearn/SoloLearnJava.png",
+      tags: [TAGS.DATOS, TAGS.PROGRAMADOR],
+    },
+  ];
