c3b5b4cd6fb32f44c2a2d4e0522b2313378e894b
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2024-05-23 04:22:48 +0000

    updated data

--- src/data/data.js
+++ src/data/data.js
@@ -1,18 +1,58 @@
 
 export const EXPERIENCIE = [
-    {
-        date: "1998",
-        title: "PHP 3",
-        company: "v 3.0",
-        description:
-          "PHP 3 fue lanzado con el apoyo de Andi Gutmans y Zeev Suraski. Esta versión incluyó mejoras en el rendimiento y la modularidad del lenguaje.",
-        link: 'https://www.php.net/docs.php'
-        },
-    {
-        date: "1998",
-        title: "PHP 3",
-        company: "PHP",
-        description:
-          "PHP 3 fue lanzado con el apoyo de Andi Gutmans y Zeev Suraski. Esta versión incluyó mejoras en el rendimiento y la modularidad del lenguaje.",
-      },
-  ];
+  {
+    date: "1994",
+    title: "ORIGENES DEL PHP",
+    company: "v 1.0",
+    description:
+      "PHP fue creado en 1994 por Rasmus Lerdorf, quien inicialmente desarrolló un conjunto de scripts CGI escritos en C conocidos como ´Personal Home Page Tools´ (Herramientas para Páginas Personales). Estos scripts se utilizaron para administrar su currículum en línea y para rastrear las visitas a su sitio web. Con el tiempo, Lerdorf incorporó características de Perl y lanzó PHP/FI (Form Interpreter), lo que marcó el comienzo del lenguaje PHP.",
+    link: 'https://www.php.net/docs.php'
+  },
+
+  {
+    date: "1998",
+    title: "PHP 3",
+    company: "v 3.0",
+    description:
+      "PHP 3 fue lanzado con el apoyo de Andi Gutmans y Zeev Suraski. Esta versión incluyó mejoras en el rendimiento y la modularidad del lenguaje.",
+    link: 'https://www.php.net/docs.php'
+  },
+
+  {
+    date: "2000",
+    title: "PHP 4",
+    company: "v 4.0",
+    description:
+      "PHP 4 se lanzó en el 2000 con mejoras en el rendimiento y la introducción del motor de scripting Zend, desarrollado por Gutmans y Suraski.",
+    link: 'https://www.php.net/docs.php'
+  },
+
+  {
+    date: "2004",
+    title: "PHP 5",
+    company: "v 5.0",
+    description:
+      "Lanzado en 2004, PHP 5 introdujo características de programación orientada a objetos, mejoras en el rendimiento y soporte para funciones avanzadas de XML.",
+    link: 'https://www.php.net/docs.php'
+  },
+
+  {
+    date: "2015",
+    title: "PHP 7",
+    company: "v 7.0",
+    description:
+      "-En 2015, se lanzó PHP 7, que mejoró significativamente el rendimiento y la eficiencia del lenguaje. También se introdujeron características como la declaración de tipos escalares y el operador de fusión de null.",
+    link: 'https://www.php.net/docs.php'
+  },
+
+  {
+    date: "2024",
+    title: "PHP 8",
+    company: "",
+    description:
+      "La última versión de PHP, es PHP 8.0 Esta versión incluye características como atributos, unión de tipos, expresiones match y mejoras en el rendimiento. PHP sigue siendo ampliamente utilizado en el desarrollo web, siendo empleado en aplicaciones populares y plataformas de comercio electrónico como Magento y WooCommerce",
+    link: 'https://www.php.net/docs.php'
+  },
+
+
+];
