b23a7c534a7939fb74f2447f3bc3b671d0823b8f
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2024-05-23 02:35:09 +0000

    clear proyect

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+
+export const EXPERIENCIE = [
+    {
+        date: "1998",
+        title: "PHP 3",
+        company: "PHP",
+        description:
+          "PHP 3 fue lanzado con el apoyo de Andi Gutmans y Zeev Suraski. Esta versión incluyó mejoras en el rendimiento y la modularidad del lenguaje.",
+      },
+    {
+        date: "1998",
+        title: "PHP 3",
+        company: "PHP",
+        description:
+          "PHP 3 fue lanzado con el apoyo de Andi Gutmans y Zeev Suraski. Esta versión incluyó mejoras en el rendimiento y la modularidad del lenguaje.",
+      },
+  ];
